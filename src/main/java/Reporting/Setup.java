0b36981eb76230cf5378d784c79f7e9db721e548
35549 <a35549@L-CSPL-IN-35549.local>
2025-01-28 17:41:08 +0530

    playwright commit

--- src/main/java/Reporting/Setup.java
+++ src/main/java/Reporting/Setup.java
@@ -1,59 +1,59 @@
-package Reporting;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
-
-import java.io.File;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-public class Setup implements ITestListener {
-
-    private static ExtentReports extentReports;
-    public static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
-    public void onStart(ITestContext context)
-    {
-
-        String fileName = ExtentReportManager.getReportNameWithTime();
-
-        String fullPath = System.getProperty("user.dir") + "/reports/" + fileName;
-
-        extentReports = ExtentReportManager.createInstances(fullPath,"Test API automation report","Test Execution Report");
-
-    }
-
-    public void onFinish(ITestContext context)
-    {
-
-        if(extentReports != null)
-        {
-            extentReports.flush();
-        }
-        else {
-            System.out.println("extentReports is null");
-        }
-
-
-    }
-
-    public void onTestStart(ITestResult result)
-    {
-
-        ExtentTest test = extentReports.createTest("Test name "+ result.getMethod().getMethodName());
-        extentTest.set(test);
-    }
-
-    public void onTestFailure(ITestResult result)
-    {
-        ExtentReportManager.logFailDetails(result.getThrowable().getMessage());
-
-        String printTrace = Arrays.toString(result.getThrowable().getStackTrace());
-        printTrace = printTrace.replaceAll(",", "<br>");
-        ExtentReportManager.logFailDetails(printTrace);
-    }
-
-
-}
+//package Reporting;
+//
+//import com.aventstack.extentreports.ExtentReports;
+//import com.aventstack.extentreports.ExtentTest;
+//import org.testng.ITestContext;
+//import org.testng.ITestListener;
+//import org.testng.ITestResult;
+//
+//import java.io.File;
+//import java.lang.reflect.Array;
+//import java.util.Arrays;
+//
+//public class Setup implements ITestListener {
+//
+//    private static ExtentReports extentReports;
+//    public static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+//    public void onStart(ITestContext context)
+//    {
+//
+//        String fileName = ExtentReportManager.getReportNameWithTime();
+//
+//        String fullPath = System.getProperty("user.dir") + "/reports/" + fileName;
+//
+//        extentReports = ExtentReportManager.createInstances(fullPath,"Test API automation report","Test Execution Report");
+//
+//    }
+//
+//    public void onFinish(ITestContext context)
+//    {
+//
+//        if(extentReports != null)
+//        {
+//            extentReports.flush();
+//        }
+//        else {
+//            System.out.println("extentReports is null");
+//        }
+//
+//
+//    }
+//
+//    public void onTestStart(ITestResult result)
+//    {
+//
+//        ExtentTest test = extentReports.createTest("Test name "+ result.getMethod().getMethodName());
+//        extentTest.set(test);
+//    }
+//
+//    public void onTestFailure(ITestResult result)
+//    {
+//        ExtentReportManager.logFailDetails(result.getThrowable().getMessage());
+//
+//        String printTrace = Arrays.toString(result.getThrowable().getStackTrace());
+//        printTrace = printTrace.replaceAll(",", "<br>");
+//        ExtentReportManager.logFailDetails(printTrace);
+//    }
+//
+//
+//}
---
+++ src/test/java/WebAutomation/Homepage.java
@@ -0,0 +1,26 @@
+package WebAutomation;
+
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
+
+public class Homepage {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+            page.navigate("https://www.fourdoor.com/gurugram");
+            page.locator("#openloginnudge").first().click();
+            page.locator("role=link[name='My cars My cars']").click();
+            page.locator("role=button[name='Add new car']").click();
+            page.locator("role=textbox[name='Enter your car number']").click();
+            page.locator("role=textbox[name='Enter your car number']").fill("sb fj hbehbfh");
+            page.locator("role=button[name='Add car']").click();
+            page.locator("role=button[name='Close']").click();
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot.png")));
+            System.out.println("Screenshot saved as screenshot.png");
+            browser.close();
+        }
+    }
+}
