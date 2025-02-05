9539342f152e697786513ffba5f515674ee87132
35549 <a35549@L-CSPL-IN-35549.local>
2025-02-05 18:31:40 +0530

    java

---
+++ src/test/java/WebAutomation/Login.java
@@ -0,0 +1,36 @@
+package WebAutomation;
+
+import io.reactivex.rxjava3.internal.operators.maybe.MaybeZipArray;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Login {
+    public static void main(String[] args) {
+        // addtion of two martix
+     /*   int[][] Marks1 = {{9,3,4},
+                {8,4,9}};
+        int[][] Marks2 = {{9,5,9},
+                {3,7,9}};
+        int [][] result = {{0,0,0},{0,0,0}};
+        for (int i = 0; i < Marks1.length; i++) {
+            for (int j = 0; j < Marks1[i].length; j++) {
+                result[i][j] = Marks1[i][j] + Marks2[i][j];
+         System.out.print(result[i][j] + " ");
+
+            }
+            System.out.println("");
+
+        }*/
+
+        String [] marks ={"abcd","cdef"};
+        for (int i = marks.length-1 ; i >= 0 ; i-- ){
+            System.out.print(" " + marks[i]);
+        }
+
+
+
+    }
+}
+
+
